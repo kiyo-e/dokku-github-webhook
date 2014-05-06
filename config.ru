65d845283bb69212f11a349dd432d2b934992dba
mba <mba>
2014-05-06 21:36:48 +0900

    for daemon

--- config.ru
+++
@@ -1,3 +0,0 @@
-
-require './worker.rb'
-run Worker
--- worker.rb
+++ worker.rb
@@ -1,17 +1,17 @@
+#!/usr/bin/env ruby
 require 'sinatra'
 require 'json'
 
 dir = File.dirname(__FILE__)
 COMMAND_SCRIPT = "#{dir}/commands"
 
-class Worker < Sinatra::Base
-  set :environment, :production
+set :environment, :production
+set :port, 9292
 
-  post "/" do
-    params = JSON.parse( request.body.read )
-    app = params["repository"]["name"]
-    url = params["repository"]["url"]
-    ref = params["ref"]
-    system("#{COMMAND_SCRIPT} webhook #{app} #{ref} #{url}")
-  end
+post "/" do
+  params = JSON.parse( request.body.read )
+  app = params["repository"]["name"]
+  url = params["repository"]["url"]
+  ref = params["ref"]
+  system("#{COMMAND_SCRIPT} webhook #{app} #{ref} #{url}")
 end
