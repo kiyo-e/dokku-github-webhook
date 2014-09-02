6f67353ae5e48d07abb0004ece850bac5f90ac1b
mba <mba>
2014-09-02 14:39:36 +0900

    Revert "margeに対応"
    This reverts commit f076302f854ddf050b081ab47ff2878a8b15a47d.

--- worker.rb
+++ worker.rb
@@ -10,20 +10,8 @@ set :port, 9292
 
 post "/" do
   params = JSON.parse( request.body.read )
-
-  if params["pull_request"]
-    @app = params["pull_request"]["head"]["repo"]["name"]
-    @url = params["pull_request"]["head"]["repo"]["ssh_url"]
-    @ref = "refs/heads/" + params["pull_request"]["head"]["ref"]
-  else
-    @app = params["repository"]["name"]
-    @url = params["repository"]["ssh_url"]
-    @ref = params["ref"]
-  end
-
-  if params["action"] == "closed" or params["deleted"] == "false"
-    system("dokku delete #{@app}")
-  else
-    system("#{COMMAND_SCRIPT} webhook #{@app} #{@ref} #{@url}")
-  end
+  app = params["repository"]["name"]
+  url = params["repository"]["url"]
+  ref = params["ref"]
+  system("#{COMMAND_SCRIPT} webhook #{app} #{ref} #{url}")
 end
