42d3836b84590bbbe8384158d154fb6ed6bbaf43
kiyo-e <kiyo-e@users.noreply.github.com>
2016-08-18 13:34:53 +0900

    small fix

--- worker.rb
+++ worker.rb
@@ -1,29 +1,48 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 require 'json'
+require 'eventmachine'
+class MyApp < Sinatra::Base
+  @@jobs = []
 
-dir = File.dirname(__FILE__)
-COMMAND_SCRIPT = "#{dir}/commands"
+  dir = File.dirname(__FILE__)
+  COMMAND_SCRIPT = "#{dir}/commands"
 
-set :environment, :production
-set :port, 9292
+  set :environment, :production
+  set :port, 9292
 
-post "/" do
-  params = JSON.parse( request.body.read )
+  post "/" do
+    params = JSON.parse( request.body.read )
 
-  if params["pull_request"]
-    @app = params["pull_request"]["head"]["repo"]["name"]
-    @url = params["pull_request"]["head"]["repo"]["ssh_url"]
-    @ref = "refs/heads/" + params["pull_request"]["head"]["ref"]
-  else
-    @app = params["repository"]["name"]
-    @url = params["repository"]["ssh_url"]
-    @ref = params["ref"]
+    if params["pull_request"]
+      @app = params["pull_request"]["head"]["repo"]["name"]
+      @url = params["pull_request"]["head"]["repo"]["ssh_url"]
+      @ref = "refs/heads/" + params["pull_request"]["head"]["ref"]
+    else
+      @app = params["repository"]["name"]
+      @url = params["repository"]["ssh_url"]
+      @ref = params["ref"]
+    end
+
+    command = if params["action"] == "closed" or params["deleted"] == "true"
+                "#{COMMAND_SCRIPT} delete #{@app} #{@ref} #{@url}"
+              elsif %w( opened reopened synchronized  ).include?(params["action"])
+                "#{COMMAND_SCRIPT} webhook #{@app} #{@ref} #{@url}"
+              end
+
+    @@jobs.push command
+    status 200 && return
   end
 
-  if params["action"] == "closed" or params["deleted"] == "true"
-    system("#{COMMAND_SCRIPT} delete #{@app} #{@ref} #{@url}")
-  elsif %w( opened reopened synchronized  ).include?(params["action"])
-    system("#{COMMAND_SCRIPT} webhook #{@app} #{@ref} #{@url}")
+  EM::defer do
+    loop do
+      sleep 5
+      next if @@jobs.empty?
+      job = @@jobs.shift ## ジョブ1つ取り出す
+      ## job処理する
+      system(job)
+    end
   end
+
+  run! if app_file == $0
 end
